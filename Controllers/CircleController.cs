86dee445fe0e213b047b5ed99bfa0b41fdc888af
quang938848 <quangcsgo2005@gmail.com>
2025-02-12 15:41:39 +0700

    vidu2

---
+++ Controllers/CircleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vidu2.Controllers
+{
+    [ApiController]
+    [Route("api/Circle")]
+    public class CircleController : ControllerBase
+    {
+        [HttpGet("cv_dt")]
+        public IActionResult GetCircleInfo(double rr)
+        {
+            if (rr <= 0)
+            {
+                return BadRequest("Bán kính phải lớn hơn 0");
+            }
+
+            var result = new
+            {
+                dien_tich = Math.PI * rr * rr,
+                chu_vi = 2 * Math.PI * rr,
+                duong_kinh = 2 * rr
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vidu2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
